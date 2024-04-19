ab4b6ee4b2621483f0d45210444d911884b4621b
Kartik <whonalekartik11@gmail.com>
2024-04-19 12:36:04 +0530

    changed some css

--- src/app/component/Login.jsx
+++ src/app/component/Login.jsx
@@ -4,6 +4,7 @@ import "@/app/component/css/Login.css";
 function Login() {
   return (
     <>
+            <div className='bodyone'> 
             <div className ="login1">
                 <form className ="form">
                     <p className ="form-title">Login</p>
@@ -11,12 +12,13 @@ function Login() {
                         <input type="text" className ="input_c"id="username" placeholder="Username" required/>
                     </div>
 
-                    <div class="input_c">
+                    <div className="input_c">
                         <input type="password" className ="input_c"id="password" placeholder="Password"/>        
                     </div><br/>
                         
                     <input type="button" value="Login" className ="submit" onclick="validateLogin()"/>
                 </form>
+            </div>
             </div>
 
     </>
